dbd0e0c37212036e6c7ab12b681a330b178f8ce6
LizGaubatz <gaubatz14@outlook.com>
2022-02-07 14:27:48 -0800

    commit

---
+++ fundamentals/essentials/collectionsPractice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace collectionsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] numArr = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            // Console.WriteLine(numArr[0]);
+        }
+    }
+}
