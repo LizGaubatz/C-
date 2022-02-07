696a8a4dba3b9456e41e7022677a81cab63e0276
LizGaubatz <gaubatz14@outlook.com>
2022-02-07 13:22:28 -0800

    commit

--- fundamentals/essentials/fundamentals1/Program.cs
+++ fundamentals/essentials/fundamentals1/Program.cs
@@ -6,7 +6,50 @@ namespace fundamentals1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // LOOP ONE
+            // for(int i=1; i<256; i++) {
+            //     Console.WriteLine(i);
+            // }
+            // LOOP TWO
+            // for(int i=0; i < 101; i++){
+            //     if (!(i % 3 == 0 && i % 5 == 0) && (i % 3 == 0 || i % 5 == 0)){
+            //         Console.WriteLine(i);
+            //     }
+            // }
+            // LOOP THREE
+            // for (int i = 0; i < 101; i++)
+            // {
+            //     if ((i % 3 == 0) && !(i % 3 == 0 && i % 5 == 0))
+            //     {
+            //         Console.WriteLine("Fizz");
+            //     }
+            //     else if ((i % 5 == 0) && !(i % 3 == 0 && i % 5 == 0))
+            //     {
+            //         Console.WriteLine("Buzz");
+            //     }
+            //     else if (i % 3 == 0 && i % 5 == 0)
+            //     {
+            //         Console.WriteLine("FizzBuzz");
+            //     }
+            // }
+            // WHILE LOOP
+            // int i = 0;
+            // while (i < 101)
+            // {
+            //     if ((i % 3 == 0) && !(i % 3 == 0 && i % 5 == 0))
+            //     {
+            //         Console.WriteLine("Fizz");
+            //     }
+            //     else if ((i % 5 == 0) && !(i % 3 == 0 && i % 5 == 0))
+            //     {
+            //         Console.WriteLine("Buzz");
+            //     }
+            //     else if (i % 3 == 0 && i % 5 == 0)
+            //     {
+            //         Console.WriteLine("FizzBuzz");
+            //     }
+            //     i++;
         }
     }
 }
+
