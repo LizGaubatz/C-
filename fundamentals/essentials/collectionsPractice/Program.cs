07424c1fa9c4bf20dfbbd526876a38462055e30b
LizGaubatz <gaubatz14@outlook.com>
2022-02-07 19:11:02 -0800

    commit

--- fundamentals/essentials/collectionsPractice/Program.cs
+++ fundamentals/essentials/collectionsPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace collectionsPractice
 {
@@ -6,12 +7,49 @@ namespace collectionsPractice
     {
         static void Main(string[] args)
         {
-            // int[] numArr = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
-            // Console.WriteLine(numArr[0]);
-            int[] numArray = new int[];
-            for (i = 1; i < 10; i++){
-                numArr = numArr + i;
+            int[] intArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+
+            string[] names = { "Tim", "Martin", "Nikki", "Sara" };
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            bool[] boolArr = new bool[11];
+            for (int i = 0; i < boolArr.Length; i += 2)
+            {
+                boolArr[i] = true;
+                Console.WriteLine(boolArr[i]);
+            }
+
+            List<string> flavors = new List<string>();
+            flavors.Add("Vanilla");
+            flavors.Add("Strawberry");
+            flavors.Add("Chocolate");
+            flavors.Add("Raspberry");
+            flavors.Add("Fudge");
+            Console.WriteLine(flavors);
+            Console.WriteLine(flavors.Count);
+            Console.WriteLine(flavors[2]);
+            flavors.RemoveAt(2);
+            Console.WriteLine(flavors.Count);
+
+            Random random = new Random();
+            Dictionary<string, string> person = new Dictionary<string, string>();
+            for (int i = 0; i < names.Length; i++)
+            {
+                person.Add(names[i], flavors[random.Next(flavors.Count)]);
+            }
+
+            foreach (KeyValuePair<string, string> entry in person)
+            {
+                Console.WriteLine(entry.Key + " - " + entry.Value);
             }
         }
     }
 }
+
