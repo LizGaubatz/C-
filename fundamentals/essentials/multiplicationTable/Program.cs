c2e68bef44e33837446cb5952c058f28bc6c4ade
LizGaubatz <gaubatz14@outlook.com>
2022-02-07 19:40:48 -0800

    commit

---
+++ fundamentals/essentials/multiplicationTable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace multiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int[,] mTable = new int[10, 10];
+        for (int row = 0; row < 10; row++)
+            for (int col = 0; col < 10; col++)
+            {
+                mTable[row, col] = (row + 1) * (col + 1);
+                Console.Write(mTable[row,col]);
+                if (col < 9) Console.Write("\t");
+                else Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
