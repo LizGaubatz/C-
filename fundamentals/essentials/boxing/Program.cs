e620eee6b227bed08a202093c1dcbe78610e241c
LizGaubatz <gaubatz14@outlook.com>
2022-02-08 16:00:29 -0800

    commit

---
+++ fundamentals/essentials/boxing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<object> newList = new List<object>();
+            newList.Add(7);
+            newList.Add(28);
+            newList.Add(-1);
+            newList.Add(true);
+            newList.Add("chair");
+
+            System.Console.WriteLine(newList);
+
+            int num = 0;
+            foreach (var obj in newList)
+            {
+                System.Console.WriteLine(obj);
+                if(obj is int) {
+                    num += (int) obj;
+                }
+            }
+            System.Console.WriteLine(num);
+        }
+    }
+}
--- fundamentals/essentials/puzzle/Program.cs
+++ fundamentals/essentials/puzzle/Program.cs
@@ -6,7 +6,7 @@ namespace puzzle
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello World!");
         }
     }
 }
