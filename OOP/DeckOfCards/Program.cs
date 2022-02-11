dec832b5833e8e6601491cb3878fd1d35ee885ca
LizGaubatz <gaubatz14@outlook.com>
2022-02-11 14:21:34 -0800

    commit

--- OOP/DeckOfCards/Program.cs
+++ OOP/DeckOfCards/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace DeckOfCards
@@ -9,171 +9,168 @@ namespace DeckOfCards
         static void Main(string[] args)
         {
 
-// TESTING __________________________________>
+    // TESTING __________________________________>
 
-            // Card testCard = new Card("Queen", "Hearts", 12);
-            // Card testCard2 = new Card()
-            // {
-            //     Name = "Queen",
-            //     Suite = "Hearts",
-            //     Value = 12
-            // };
-            // Deck deck = new Deck();
-            // Player player = new Player("Test Dude");
-            // deck.Print();
-            // deck.Shuffle();
-            // Console.WriteLine(deck.Deal());
-            // Console.WriteLine(deck.Deal());
-            // Console.WriteLine(deck.Deal());
+                // Card testCard = new Card("Queen", "Hearts", 12);
+                // Card testCard2 = new Card()
+                // {
+                //     Name = "Queen",
+                //     Suite = "Hearts",
+                //     Value = 12
+                // };
+                Deck deck = new Deck();
+                Console.WriteLine(deck);
+                // Player player = new Player("Test Dude");
+                // deck.Print();
+                // deck.Shuffle();
+                // Console.WriteLine(deck.Deal());
+                // Console.WriteLine(deck.Deal());
+                // Console.WriteLine(deck.Deal());
 
-            // Console.WriteLine(player.Draw(deck));
-            // Console.WriteLine(player.Draw(deck));
-            // Console.WriteLine(player.Draw(deck));
-            // Console.WriteLine(player.Hand.Count);
-            // Console.WriteLine(player.Discard(0));
-            // Console.WriteLine(player.Hand.Count);
-        }
-
-
-// CARD CLASS__________________________________>
-
-    public class Card
-    {
-        public string Name { get; set; }
-        public string Suit { get; set; }
-        public int Value { get; set; }
-        public Card() { }
-        public Card(string name, string suit, int value)
-        {
-            Name = name;
-            Suit = suit;
-            Value = value;
-        }
-        public override string ToString()
-        {
-            return $@"
-        Name:  {Name} 
-        Suite: {Suit}
-        Value: {Value}";
-        }
+                // Console.WriteLine(player.Draw(deck));
+                // Console.WriteLine(player.Draw(deck));
+                // Console.WriteLine(player.Draw(deck));
+                // Console.WriteLine(player.Hand.Count);
+                // Console.WriteLine(player.Discard(0));
+                // Console.WriteLine(player.Hand.Count);
             }
 
+    // CARD CLASS__________________________________>
 
-
-// DECK CLASS __________________________________>
-
-public class Deck
-    {
-        public List<Card> Cards { get; set; } = new List<Card>();
-        public Deck()
-        {
-            Reset();
-        }
-        public Deck Reset()
-        {
-            Cards = new List<Card>();
-            string[] suits =
+            public class Card
             {
-                "Hearts",
-                "Diamonds",
-                "Spades",
-                "Clubs"
-            };
-            Dictionary<int, string> faceCardNames = new Dictionary<int, string>()
-            {
-                { 1, "Ace" },
-                { 11, "Jack" },
-                { 12, "Queen" },
-                { 13, "King" },
-            };
-            foreach (string suit in suits)
-            {
-                for (int i = 1; i < 14; i++)
+                public string Name { get; set; }
+                public string Suit { get; set; }
+                public int Value { get; set; }
+                public Card(string name, string suit, int value)
                 {
-                    string name = i.ToString();
-                    if (faceCardNames.ContainsKey(i))
-                    {
-                        name = faceCardNames[i];
-                    }
-                    Card card = new Card(name, suit, i);
-                    Cards.Add(card);
+                    Name = name;
+                    Suit = suit;
+                    Value = value;
+                }
+                public override string ToString()
+                {
+                    return $@"
+                Name:  {Name} 
+                Suite: {Suit}
+                Value: {Value}";
                 }
             }
-            return this;
-        }
+            
 
-        public Card Deal()
+
+    // DECK CLASS __________________________________>
+
+        public class Deck
         {
-            if (Cards.Count > 0)
+            public List<Card> Cards { get; set; } = new List<Card>();
+            public Deck()
             {
-                Card topCard = Cards[Cards.Count - 1];
-                Cards.RemoveAt(Cards.Count - 1);
-                return topCard;
+                Reset();
             }
-            return null;
-        }
-
-        public Deck Shuffle()
-        {
-            Random random = new Random();
-            for (int i = 0; i < Cards.Count; i++)
+            public Deck Reset()
             {
-                int randomIndex = random.Next(Cards.Count);
-                Card temp = Cards[i];
-                Cards[i] = Cards[randomIndex];
-                Cards[randomIndex] = temp;
+                Cards = new List<Card>();
+                string[] suits =
+                {
+                    "Hearts",
+                    "Diamonds",
+                    "Spades",
+                    "Clubs"
+                };
+                Dictionary<int, string> faceCardNames = new Dictionary<int, string>()
+                {
+                    { 1, "Ace" },
+                    { 11, "Jack" },
+                    { 12, "Queen" },
+                    { 13, "King" },
+                };
+                foreach (string suit in suits)
+                {
+                    for (int i = 1; i < 14; i++)
+                    {
+                        string name = i.ToString();
+                        if (faceCardNames.ContainsKey(i))
+                        {
+                            name = faceCardNames[i];
+                        }
+                        Card card = new Card(name, suit, i);
+                        Cards.Add(card);
+                    }
+                }
+                return this;
             }
-            return this;
-        }
 
-        public void Print()
-        {
-            string printableDeck = "";
-            foreach (Card card in Cards)
+            public Card Deal()
             {
-                // Since printableDeck is a string when we += card C# knows
-                // to execute the card's ToString method.
-                printableDeck += card + "\n";
-            }
-            Console.WriteLine(printableDeck);
-        }
-    }
-}
-
-// PLAYER CLASS __________________________________>
-
-public class Player
-    {
-        public string Name { get; set; }
-        public List<Card> Hand { get; set; } = new List<Card>();
-        public Player(string name)
-        {
-            Name = name;
-        }
-
-        public Card Draw(Deck deck)
-        {
-            Card dealtCard = deck.Deal();
-            if (dealtCard != null)
-            {
-                Hand.Add(dealtCard);
-            }
-            return dealtCard;
-        }
-        public Card Discard(int i)
-        {
-            if (i < 0 || i >= Hand.Count)
-            {
+                if (Cards.Count > 0)
+                {
+                    Card topCard = Cards[Cards.Count - 1];
+                    Cards.RemoveAt(Cards.Count - 1);
+                    return topCard;
+                }
                 return null;
             }
-            Card cardToRemove = Hand[i];
-            Hand.Remove(cardToRemove);
-            return cardToRemove;
+
+            public Deck Shuffle()
+            {
+                Random random = new Random();
+                for (int i = 0; i < Cards.Count; i++)
+                {
+                    int randomIndex = random.Next(Cards.Count);
+                    Card temp = Cards[i];
+                    Cards[i] = Cards[randomIndex];
+                    Cards[randomIndex] = temp;
+                }
+                return this;
+            }
+
+            public void Print()
+            {
+                string printableDeck = "";
+                foreach (Card card in Cards)
+                {
+                    // Since printableDeck is a string when we += card C# knows
+                    // to execute the card's ToString method.
+                    printableDeck += card + "\n";
+                }
+                Console.WriteLine(printableDeck);
+            }
+        }
+
+    // PLAYER CLASS __________________________________>
+
+    public class Player
+        {
+            public string Name { get; set; }
+            public List<Card> Hand { get; set; } = new List<Card>();
+            public Player(string name)
+            {
+                Name = name;
+            }
+
+            public Card Draw(Deck deck)
+            {
+                Card dealtCard = deck.Deal();
+                if (dealtCard != null)
+                {
+                    Hand.Add(dealtCard);
+                }
+                return dealtCard;
+            }
+            public Card Discard(int i)
+            {
+                if (i < 0 || i >= Hand.Count)
+                {
+                    return null;
+                }
+                Card cardToRemove = Hand[i];
+                Hand.Remove(cardToRemove);
+                return cardToRemove;
             }
         }
     }
 }
-
 
 
 
@@ -297,4 +294,3 @@ public class Player
         //         }else{
         //             return null;
         //         }
-}
