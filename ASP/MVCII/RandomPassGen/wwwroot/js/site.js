4b2eb65f3334541c3949807cfdd501d105fb9b17
LizGaubatz <gaubatz14@outlook.com>
2022-02-18 17:14:41 -0800

    commit

--- ASP/MVCII/RandomPassGen/wwwroot/js/site.js
+++ ASP/MVCII/RandomPassGen/wwwroot/js/site.js
@@ -1,4 +1,19 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Write your JavaScript word.
+
+function getPassword(){
+    fetch("http://localhost:5000/ajax")
+        .then(res => res.json())
+        .then(res => {
+            document.getElementById("PCNUM").innerHTML = `Random password (password #${res.num})`;
+            document.getElementById("PCCODE").innerHTML = res.password;
+        })
+}
+
+$(document).ready(function(){
+    $("#generateButton").click(function(){
+        getPassword()
+    })
+})
